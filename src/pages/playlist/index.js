8890e540f0ad6f638f2d2cfe3c321747ba4120af
Lucas <lucas@alternativainternet.com>
2019-05-08 09:28:56 -0300

    tocando a musica

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Creators as PlaylistDetailsActions } from "../../store/ducks/playlistDetails";
+import { Creators as PlayerActions } from "../../store/ducks/player";
 
 import { Container, Header, SongList } from "./styles";
 import Loading from "../../components/loading";
@@ -34,7 +35,8 @@ class Playlist extends Component {
         )
       }),
       loading: PropTypes.bool
-    }).isRequired
+    }).isRequired,
+    loadSong: PropTypes.func.isRequired
   };
 
   componentDidMount() {
@@ -87,7 +89,10 @@ class Playlist extends Component {
               </tr>
             ) : (
               playlist.songs.map(song => (
-                <tr key={song.id}>
+                <tr
+                  key={song.id}
+                  onDoubleClick={() => this.props.loadSong(song)}
+                >
                   <td>
                     <img src={PlusIcon} alt="adicionar" />
                   </td>
@@ -120,7 +125,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(PlaylistDetailsActions, dispatch);
+  bindActionCreators({ ...PlaylistDetailsActions, ...PlayerActions }, dispatch);
 
 export default connect(
   mapStateToProps,
